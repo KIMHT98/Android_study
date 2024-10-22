cc449be8777144113daef568c2a16b3e3291d7d9
kht19684 <kht19684@gmail.com>
2024-10-23 08:31:08 +0900

    FINISH:CH05

---
+++ Ch_07_Shoppingmall/app/src/main/java/com/example/ch_07_shoppingmall/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ch_07_shoppingmall
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ch_07_shoppingmall.ui.theme.Ch_07_ShoppingmallTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ch_07_ShoppingmallTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ch_07_ShoppingmallTheme {
+        Greeting("Android")
+    }
+}
